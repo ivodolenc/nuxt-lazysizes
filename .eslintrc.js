3048c9740a7fe23b62c026a3e9f95756f5a09e8a
Ivo Dolenc <ivodolenc@gmail.com>
2022-03-31 14:25:21 +0200

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,23 +7,11 @@ module.exports = {
   },
 
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     sourceType: 'module',
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    requireConfigFile: false
   },
 
-  extends: ['@nuxtjs', 'eslint-config-prettier'],
-
-  rules: {
-    'no-debugger': 'warn',
-    'no-console': 'warn',
-
-    'vue/html-self-closing': 'off',
-    'vue/html-indent': 'off',
-
-    'vue/max-attributes-per-line': 'off',
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off'
-  }
+  extends: ['eslint-config-prettier']
 }
